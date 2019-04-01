20c00492f6838e5507a9724d431149af844f30cb
Christopher Hermansson Kekev <31985025+Jackpotdante@users.noreply.github.com>
2019-04-01 14:34:10 +0200

    Update to files. Still not finished.
    Issues with Firebase initiation.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 // Firebase Initialize
-var config = {
+const config = {
     apiKey: "AIzaSyAXT-UnNRciX2C7wEqYb_i57IyXaAsYwO0",
     authDomain: "tekken-live-frame.firebaseapp.com",
     databaseURL: "https://tekken-live-frame.firebaseio.com",
@@ -9,3 +9,49 @@ var config = {
     messagingSenderId: "61792486479"
   };
   firebase.initializeApp(config);
+
+  //Database for Framedata
+
+  const preDataTable =document.getElementById("dataTable");
+  
+  const dbRefObject=firebase.database().ref().child('dataTable');
+
+  //Sync object changes
+
+  dbRefObject.on("value",snap=> console.log(snap.val()));
+
+  //Hide Frame Data 
+
+  function hideFrames() {
+    var x = document.getElementById("dataTable");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  //
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
